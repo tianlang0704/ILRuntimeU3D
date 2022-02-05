dbafcf0e1a8dc9ef35ecef67e057e2e8f35af91c
CMonk <tianlang0704@gmail.com>
2022-02-06 01:49:58 +0800

    CI

---
+++ ILRuntimeDemo/Assets/Scripts/Editor/PrebuildProcessor.cs
@@ -0,0 +1,16 @@
+using CSObjectWrapEditor;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+class PrebuildProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("MyCustomBuildProcessor.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+        Generator.ClearAll();
+        Generator.GenAll();
+    }
+}
